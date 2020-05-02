7def6187387f9d652eb8594640fa826a5d1fc03d
hmm-mayank <mayankanildwivedi@gmail.com>
2020-05-03 00:24:37 +0530

    Yellow box warning Removed

--- App/Views/Entry.tsx
+++ App/Views/Entry.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Provider from '../Modules/context';
 import AppLayout from './AppLayout/AppLayout';
 const EntryComponent = () => {
+  console.disableYellowBox = true; // for disabling the Yello box warning
   const initialValue = { rating: 2 };
   return (
     <Provider value={initialValue}>
